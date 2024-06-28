028c112b3a25cdbf2716f108976b4cf520aa2753
omar saraqbi <omar.saraqbi@hotmail.com>
2024-06-28 21:25:26 +0300

    new file:   AsciiToArabicConvertor.sln 	new file:   AsciiToArabicConvertor/AsciiToArabicConvertor.csproj 	new file:   AsciiToArabicConvertor/Program.cs 	new file:   AsciiToArabicConvertor/asciiToArabic.cs

---
+++ AsciiToArabicConvertor/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AsciiToArabicConverter
+{
+    class Program
+    {
+        public static void Main()
+        {
+           asciiToArabic.Converter();           
+        }
+    }
+}
---
+++ AsciiToArabicConvertor/asciiToArabic.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Reflection;
+
+namespace AsciiToArabicConverter
+{
+    class asciiToArabic
+    {
+        public static void Converter()
+        {
+            Console.Write("أدخل رموز ASCII (مفصولة بمسافة): ");
+            string asciiInput = Console.ReadLine();
+
+            // تحويل الرموز ASCII إلى مصفوفة من الأعداد
+            string[] asciiValues = asciiInput.Split(' ');
+            string arabicText = "";
+
+            foreach (string value in asciiValues)
+            {
+                try
+                {
+                    // تحويل كل قيمة إلى حرف عربي
+                    int asciiValue = int.Parse(value);
+                    char arabicChar = (char)asciiValue;
+                    arabicText += arabicChar;
+                }
+                catch (Exception)
+                {
+                    // تجاهل القيم غير الصحيحة
+                }
+            }
+
+            Console.WriteLine("النص العربي:");
+            Console.WriteLine(arabicText);
+            Program.Main();
+        }
+    }
+}
